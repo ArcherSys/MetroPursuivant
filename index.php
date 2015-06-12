4b902308b31bb50c4753e731887b87c94bc41902
unknown <Student@PS458S-5KR6MVIOIN60.students.dc_schools.k12.dc.us>
2015-06-11 22:39:41 -0400

    Newest

--- index.php
+++ index.php
@@ -26,6 +26,12 @@ background-color: green;
 background-color: crimson;
  width: 1000px;
 }
+#traintimes td{
+background-color: lightgreen;
+color: white;
+border: 1px solid blue;
+
+}
 </style>
 <?php
 }, function(){
@@ -66,6 +72,28 @@ $.ajax({
 
 }
 });
+
+      
+      $.ajax({
+         url: 'https://api.wmata.com/Incidents.svc/json/ElevatorIncidents?' + $.param({
+         // Specify your subscription key
+         'api_key': '3bd3f9fe9c0d4c9087ff7438fe507aa7',
+         // Specify values for optional parameters, as needed
+         // StationCode: "",
+      }),
+         type: 'GET',
+      })
+      .done(function(data) {
+         
+         window.Notification.requestPermission(function(status){
+         for(var e = 0;  e < data.BusIncidents.length; e++){
+         note =  new window.Notification(data.ElevatorIncidents[e].UnitType + " Outage",{"body": data.ElevatorIncidents[e].Description});
+}
+});
+      })
+      .fail(function() {
+         alert("error");
+      });
   })
       .fail(function() {
          alert("error");
@@ -115,30 +143,51 @@ $("#businfo").html("<div class='MetroPursuivantError'>Error</div>");
       });
 }, 60000);
   $.ajax({
-         url: 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/{StationCodes}?' + $.param( {
+         url: 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/B03?' + $.param( {
          // Specify your subscription key
          'api_key': '3bd3f9fe9c0d4c9087ff7438fe507aa7',
       }),
          type: 'GET',
       })
       .done(function(data) {
-
+          $("#traintimes").append("<thead><tr></tr><tr></tr><th>Number of RailCars</th><th>Line Color</th><th>Minutes till arrival</th></tr></thead>");
           for( var k = 0; k < data.Trains.length; k++){
             console.log(data.Trains[k]);
-            $(".predictions")[k].append('<li><span class="trains-car>' + data.Trains[k].Car + "<span>" +"<span class='trains-line'>" +  data.Trains[k].Line + '</span><span class="trains-minutes">' + data.Trains[k].Min + "</span></li>");
-           
-}
-      
-       
+            $("#traintimes").append('<tr><td class="trains-destination">' + data.Trains[k].DestinationName + '</td><td class="trains-car">' + data.Trains[k].Car + " </td>" +"<td class='trains-line'> " +  data.Trains[k].Line + ' </td><td class="trains-minutes">' + data.Trains[k].Min + "</td></tr>");
+          }
+window.setInterval(function(){
+           $.ajax({
+         url: 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/B03?' + $.param( {
+         // Specify your subscription key
+         'api_key': '3bd3f9fe9c0d4c9087ff7438fe507aa7',
+      }),
+         type: 'GET',
       })
-      .fail(function() {
-         alert("error");
-      });
+      .done(function(data) {
+for( var r = 0; r < data.Trains.length; r++){
+            window.document.GetElementsByClassName("trains-destination")[r].innerHTML = data.Trains[k].DestinationName;
+            window.document.GetElementsByClassName("trains-car")[r].innerHTML = data.Trains[k].Car;
+            
+
+            window.document.GetElementsByClassName("trains-line")[r].innerHTML = data.Trains[k].Line;
+            
+            window.document.GetElementsByClassName("trains-minutes")[r].innerHTML = data.Trains[k].Min;
+}
+     }).fail(function(){
+        alert("No Data");
+});
+   
+},60000);
+ }).fail(function(){
+        alert("No Data");
+});
 });
 </script>
 <div class="MetroPursuivantHeader"><h1 id="MPH1"></h1></div>
 <input type="number" name="stopid" id="busstopid">
 <div id="businfo"><ul id="bustimes"></ul></div>
+
+<div id="traininfo"><table id="traintimes"></ul></div>
 <button id="MetroPursuivantHeaderShow"><h1>Show</h1></button>
 <div id="busincidents"></div>
 <?php
